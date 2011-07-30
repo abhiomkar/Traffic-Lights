e3d0d48722580604e16081603b6d063aa240add6
Abhinay Omkar <abhiomkar@gmail.com>
2011-07-30 18:18:02 +0000

    changed db host to local IP

--- settings.py
+++ settings.py
@@ -18,7 +18,7 @@ DATABASES = {
         'NAME': 'trlights', # Or path to database file if using sqlite3.
         'USER': 'postgres',                      # Not used with sqlite3.
         'PASSWORD': 'OpenHack#2011',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
